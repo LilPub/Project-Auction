43e120fc86cc0513e475e49445a219aad0ee1282
LilPub <hiep140701@gmail.com>
2021-10-26 21:11:45 +0700

    add

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div id="wrapper">
+        <div id="header" class="header">
+            <div id="content">
+                <header class="p-3 text-white content-top">
+                    <div class="container">
+                        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                            <img src="IMG/logoContent.png" alt="" style="width: 200px;height: 40px;margin-left: -80px;">
+
+                            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                                <li><a href="#" class="nav-link px-2 text-white">Home</a></li>
+                                <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
+                                <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
+                                <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+                                <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+                            </ul>
+
+                            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                                <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+                            </form>
+
+                            <div class="text-end">
+                                <button type="button" class="btn btn-outline-light me-2">Login</button>
+                                <button type="button" class="btn btn-outline-light me-2">Sign-up</button>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+                <div id="carouselExampleSlidesOnly" class="carousel slide content-slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active text-center">
+                            Sở hữu những sản phẩm của thời đại<br/>
+                            <u>Xem ngay !</u>
+                        </div>
+                        <div class="carousel-item text-center">
+                            Khám phá những dòng sản phẩm công nghệ<br/>
+                            <u>Xem ngay !</u>
+                        </div>
+                        <div class="carousel-item text-center">
+                            Những sản phẩm mang tính chất đột phá<br/>
+                            <u>Xem ngay !</u>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="main" class="main">
+            <div class="main-content d-flex">
+                <div class="main-content-img">
+                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="IMG/img1.jpg" class="d-block " alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="IMG/img2.jpg" class="d-block " alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="IMG/img3.jpg" class="d-block " alt="...">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="main-content-text">
+                    <h5>Chất lượng nổi bật của thế giới đồ cổ</h5>
+                    <h1>Bộ sưu tập đồ cổ tốt nhất thế giới</h1>
+                    <p>Cửa hàng đồ cổ hay cửa hàng đồ cổ là một cửa hàng bán lẻ chuyên bán đồ cổ. 
+                        Các cửa hàng đồ cổ có thể được đặt tại địa phương hoặc với sự ra đời của Internet.</p>
+                    <button>Vào xem</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
